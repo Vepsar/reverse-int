d5f3e9e901afc143f279dc453fcaae3c3913b562
Vepsar <70876483+Vepsar@users.noreply.github.com>
2020-10-10 22:22:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    var nstr = n.toString();
+    let bw = ''
+    for (var i = nstr.length; i > -1; i--) {
+        bw = bw + nstr.charAt(i)
+    }
+    if (n > 0) { return parseInt(bw) } else {
+
+        return bw.slice(0, -1)
+    }
 }
